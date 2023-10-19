b31ba13668e90bc4f1bb5bd8c300058d0f9c86fd
prubac <pawel.rubach@gmail.com>
2023-10-19 14:43:07 +0200

    Initial commit

---
+++ vars.py
@@ -0,0 +1,52 @@
+a = 26327478348548347634523572342891562346
+b = 23416734621562348979678934769027386937
+
+c = a + b
+print(type(a))
+print(f'a={a}')
+print(f'b={b}')
+#print(type(c))
+print(f'c={c}')
+
+
+af = 263274783485483476345.23572342891562346
+bf = 234167346215623489796.78934769027386937
+
+cf = af + bf
+print(type(af))
+print(f'af={af}')
+print(f'bf={bf}')
+#print(type(cf))
+print(f'cf={cf}')
+cfi = int(cf*1e+20)
+print(f'cfi={cfi}')
+
+g = 1e-2
+print(g)
+
+h = 1.79e+308
+print(f'h={h}')
+print('h={}'.format(h))
+print('h=' + str(h))
+
+
+afs = '263274783485483476345.23572342891562346'
+bfs = '234167346215623489796.78934769027386937'
+
+cfs = afs + bfs
+print(type(cfs))
+print(f'cfs={cfs}')
+
+cfs = float(afs) + float(bfs)
+print(type(cfs))
+print(f'cfs={cfs}')
+
+j = int(63636.64736)
+k = round(63636.64736)
+print(j)
+print(k)
+
+ds = '26672340'
+di = int(ds)
+print(di)
+print(type(di))
