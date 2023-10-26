14b2d71e61e0a3834386b22f28f572503a98b722
prubac <pawel.rubach@gmail.com>
2023-10-26 14:50:37 +0200

    Conditions - match

--- conditions.py
+++ conditions.py
@@ -21,3 +21,45 @@ if p ^ q:
     print('p xor q True')
 else:
     print('p xor q False')
+
+
+print('--------------')
+a = 10
+b = 10
+
+if a >= b:
+    print('a>=b')
+else:
+    print('not a>=b')
+
+if a == b:
+    print('a=b')
+else:
+    print('not a=b')
+
+#if not a == b:
+if not a != b:
+    print('a!=b')
+else:
+    print('not a!=b')
+
+s1 = 'abc'
+s2 = 'abcd'
+
+if s1 == s2:
+    print('s1=s2')
+else:
+    print('not s1=s2')
+
+y = 1000 if s1 == s2 else -1000
+print(y)
+
+s3 = 'abcb'
+
+match s3:
+    case 'ab':
+        print('s3=ab')
+    case 'abc':
+        print('s3=abc')
+    case _:
+        print('s3 is other')
