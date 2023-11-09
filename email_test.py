9f7936f9877f5699b89fff4f545ff9f8fcd5b40d
prubac <pawel.rubach@gmail.com>
2023-11-09 10:03:15 +0100

    Email exercise

--- email_test.py
+++ email_test.py
@@ -4,3 +4,8 @@ email3 = 'pawel@pl'
 email4 = 'pawel@'
 
 #TODO create a function that will test if the email is valid
+def validate_email(email):
+    #....
+    return True
+
+print(validate_email(email1))
