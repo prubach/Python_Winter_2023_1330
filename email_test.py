8a87203df64962a1fec3c96dda43a8c640d4616b
prubac <pawel.rubach@gmail.com>
2023-11-09 14:01:51 +0100

    Email exercise

--- email_test.py
+++ email_test.py
@@ -4,9 +4,13 @@ email3 = 'pawel@pl'
 email4 = 'pawel@'
 email5 = 'pawel@.pl'
 
+email_list = [email1, email2, email3, email4, email5]
 #TODO create a function that will test if the email is valid
 def validate_email(email):
     #....
     return True
 
-print(validate_email(email1))
+
+#print(validate_email(email1))
+for em in email_list:
+    print('{}: {}'.format(em, validate_email(em)))
