a3d3d78c8e783a9d9a314e6fb79ceaa53fc17622
prubac <pawel.rubach@gmail.com>
2023-11-16 14:42:58 +0100

    Matrix

---
+++ lists_2D.py
@@ -0,0 +1,14 @@
+num_list = [(3, 5), (1, 2), (3, 1), (0, 0), (4, 2)]
+x, y = num_list[1]
+print(x, y)
+
+list_2D = [(3, 5, 2), (1, 2, 0), (3, 1, 1), (0, 0, 2), (4, 2, 3)]
+for row in list_2D:
+    print(row)
+
+#TODO:
+# 1. Create a list of sums of each row
+# 2. Sum all elements of the matrix
+# 3. Create a list of sums of each column
+
+
