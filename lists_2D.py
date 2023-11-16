d984f5ec315822bf124ab93ebb1cbf9fe05e16b2
prubac <pawel.rubach@gmail.com>
2023-11-16 15:02:38 +0100

    Exercise row and column sum of matrix

--- lists_2D.py
+++ lists_2D.py
@@ -2,12 +2,16 @@ num_list = [(3, 5), (1, 2), (3, 1), (0, 0), (4, 2)]
 x, y = num_list[1]
 print(x, y)
 
-list_2D = [(3, 5, 2), (1, 2, 0), (3, 1, 1), (0, 0, 2), (4, 2, 3)]
+#list_2D = [(3, 5, 2), (1, 2, 0), (3, 1, 1), (0, 0, 2), (4, 2, 3)]
+#list_2D = ((3, 5, 2), (1, 2, 0), (3, 1, 1), (0, 0, 2), (4, 2, 3))
+list_2D = [[3, 5, 2], [1, 2, 0], [3, 1, 1], [0, 0, 2], [4, 2, 3]]
 for row in list_2D:
     print(row)
 
 #TODO:
 # 1. Create a list of sums of each row
+row_sum = [ sum(row) for row in list_2D]
+print(row_sum)
 # 2. Sum all elements of the matrix
 # 3. Create a list of sums of each column
 
