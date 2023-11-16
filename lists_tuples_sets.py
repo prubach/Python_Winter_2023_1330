a0573fa299c99dda53180e9551ea466559da3566
prubac <pawel.rubach@gmail.com>
2023-11-16 13:40:51 +0100

    2nd powers solved

---
+++ lists_tuples_sets.py
@@ -0,0 +1,2 @@
+num_list = [3, 5, 21, 7, 14, 8, 11, 3, 14, 24]
+list_2nd_powers = [ i**2 for i in num_list ]
--- play_with_numbers.py
+++ play_with_numbers.py
@@ -5,3 +5,14 @@ TODO
 1. List the subsequent 2nd powers of every element from the list
 2. Sum the 2nd powers of all elements in the list
 '''
+sum_els = 0.0
+for i in num_list:
+    print(i**2)
+    sum_els += (i ** 2)
+print(sum_els)
+
+print('----------------')
+list_2nd_powers = [ i**2 for i in num_list ]
+#list_2nd_powers = [ i**2 for i in num_list if i > 10]
+print(list_2nd_powers)
+print(sum(list_2nd_powers))
