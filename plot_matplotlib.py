bdfcc5eafef7beb7be96a540287fb83ee300fb05
prubac <pawel.rubach@gmail.com>
2023-11-30 14:16:09 +0100

    Plot 3D matplotlib

--- plot_matplotlib.py
+++ plot_matplotlib.py
@@ -17,3 +17,11 @@ points_3D = [[3, 5, 2], [1, 2, 0], [3, 1, 1], [0, 0, 2], [4, 2, 3]]
 xs = [i[0] for i in points_3D]
 ys = [i[1] for i in points_3D]
 zs = [i[2] for i in points_3D]
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(xs, ys, zs, marker='^')
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+plt.show()
