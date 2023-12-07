c4ce821ec8438a7481409e498cd15602a0233f15
prubac <pawel.rubach@gmail.com>
2023-12-07 13:43:34 +0100

    Added a solution by Cuong Vo

--- file_read_csv.py
+++ file_read_csv.py
@@ -8,3 +8,26 @@ for line in lines:
     print(f'{i}: {line}', end='')
 
 #TODO - read these numbers into a 2D list and then sum rows, cols and all
+
+a = []
+for line in lines:
+    tmp = line.replace('\n','').split(sep=';')
+    tmp = [int(x) for x in tmp]
+    a.append(tmp)
+
+print(a)
+
+list_row = []
+list_col = [0 for i in range(len(a[0]))]
+
+for row in a:
+    list_row.append(sum(row))
+
+print(list_row)
+print(sum(list_row))
+
+for row in a:
+    for i in range(len(row)):
+        list_col[i] += row[i]
+
+print(list_col)
