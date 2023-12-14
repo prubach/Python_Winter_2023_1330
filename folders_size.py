9b6c640fff912cbe204ff42cb3b42f931a93e130
prubac <pawel.rubach@gmail.com>
2023-12-14 14:53:53 +0100

    Solution to size of files in folder

--- folders_size.py
+++ folders_size.py
@@ -12,3 +12,17 @@ for f in os.listdir(my_folder):
 #os.remove()
 
 #TODO - given a folder please sum up the space occupied by all regular files in it
+
+# Solution by Cuong Vo
+import os
+
+folder = os.getcwd()
+
+size_sum = 0
+
+for f in os.listdir(folder):
+    tmp = os.path.join(folder,f)
+    if os.path.isfile(tmp):
+        size_sum += os.path.getsize(tmp)
+
+print(f'Total file size is {round(size_sum / 1024, 2)} KB')
