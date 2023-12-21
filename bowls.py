af02e59e02f43018b0523176dd1481ec7ad5e084
prubac <pawel.rubach@gmail.com>
2023-12-21 13:40:07 +0100

    Sum bowls using recursion

--- bowls.py
+++ bowls.py
@@ -5,7 +5,7 @@
 #       * *
 #        *
 
-n = 6
+n = 100
 #TODO given n - number of rows write a function that gives the
 # sum of all bowls
 
@@ -19,5 +19,14 @@ def sum_of_bowls_loop(n):
     sum_bowls = sum(list(range(n, 0, -1)))
     return sum_bowls
 
+
+def sum_of_bowls_recursive(n):
+    if n == 1:
+        return 1
+    else:
+        res = sum_of_bowls_recursive(n-1) + n
+        return res
+
 print('Sum of bowls using sequence: {}'.format(sum_of_bowls_sequence(n)))
-print('Sum of bowls using sequence: {}'.format(sum_of_bowls_loop(n)))
+print('Sum of bowls using loop: {}'.format(sum_of_bowls_loop(n)))
+print('Sum of bowls using recursion: {}'.format(sum_of_bowls_recursive(n)))
