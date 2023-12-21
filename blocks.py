86b9ff0ddd2b38b8bcfb674526763f28e2e79541
prubac <pawel.rubach@gmail.com>
2023-12-21 14:47:19 +0100

    Scope - visibility of variables

---
+++ blocks.py
@@ -0,0 +1,18 @@
+a = 0
+for i in range(3):
+    print(f'i={i}')
+    a = 5
+
+print('------loop done------')
+print(f'a={a}')
+print(f'i={i}')
+
+a = 3
+b = 10
+if a > 4:
+    if b > 8:
+        d = 10
+    else:
+        d = 20
+
+print(f'd={d}')
